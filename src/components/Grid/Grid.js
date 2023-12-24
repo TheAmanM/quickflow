e30433d2096ba5ee37569989a2132c2e622d618c
TheAmanM <dacyberdash@gmail.com>
2023-12-24 20:58:26 +0500

    Added ReactFlow

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Grid.css";
+import ReactFlow, { Background, ReactFlowProvider } from "reactflow";
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 50, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlowProvider>
+        <ReactFlow nodes={initialNodes} edges={initialEdges}>
+          <Background variant="dots" gap={12} size={1} />
+        </ReactFlow>
+      </ReactFlowProvider>
+    </div>
+  );
 };
 
 export default Grid;
