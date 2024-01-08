29210654303e80147c712be12efcbaeaa664a949
TheAmanM <dacyberdash@gmail.com>
2024-01-08 06:14:21 +0500

    Added top panel component

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -7,6 +7,7 @@ import ReactFlow, {
   Controls,
   MarkerType,
   MiniMap,
+  Panel,
   ReactFlowProvider,
   addEdge,
   applyEdgeChanges,
@@ -213,6 +214,7 @@ const Grid = () => {
               </svg>
             </ControlButton>
           </Controls>
+          <Panel position="top-center">top-center</Panel>
         </ReactFlow>
       </ReactFlowProvider>
     </div>
